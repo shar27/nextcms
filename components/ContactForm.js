8ebbcfc38407645b712a1ea9a88861e900c93318
shar27 <shariq.ahmed731@gmail.com>
2022-08-01 16:37:54 +0200

    success message

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,16 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function ContactUs () {
   const form = useRef();
+  const [text, setText] = useState('')
 
   const sendEmail = (e) => {
     e.preventDefault();
+
 
     // process.env.YOUR_TEMPLATE_ID
     emailjs.sendForm('service_h1h55e6', 'template_20q0qxs', form.current, process.env.NEXT_PUBLIC_YOUR_PUBLIC_KEY)
       .then((result) => {
           console.log(result.text);
+          setText('Thank you, your email has been received')
       }, (error) => {
           console.log(error.text);
       });
@@ -19,8 +22,10 @@ export default function ContactUs () {
   return (
    
 
-    <form className='border border-2 border-black h-xxl2 lg:h-96 
+    <form className='border border-2 border-black h-xxl3 lg:h-96 
+    
     w-full lg:w-2/5  text-center mt-10  p-5' ref={form} onSubmit={sendEmail}>
+      <p className='text-xl text-red-500 font-bold p-2'>{text}</p>
       <section>
       <label className='font-bold p-5'>Name</label>
       <input className='w-80 border-2 border-black  rounded-sm mb-5' type="text" name="user_name" />
