35d5c34419152285232e2fdd73b4e972bcf8ca74
shar27 <shariq.ahmed731@gmail.com>
2024-06-02 19:22:38 +0200

    text-black

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ h-20 text-xl lg:text-3xl p-5 font-bold">
 </div>
 
 <h3 className="text-2xl lg:text-4xl text-black text-center font-bold p-20">Our process</h3>
-<div className="grid grid-cols-1 lg:grid-cols-2  p-5 bg-white">
+<div className="grid grid-cols-1 lg:grid-cols-2  p-5 bg-white text-black">
 <div className="w-full max-w-lg">
   <h2 className="text-2xl lg:text-3xl mb-5">Step 1 - Research phase</h2>
 
