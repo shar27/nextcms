16e5d24dffe0e66d59bda48a27459b30b7e7d573
shar27 <shariq.ahmed731@gmail.com>
2024-06-09 14:38:05 +0200

    import wrong module

--- components/Imports.js
+++ components/Imports.js
@@ -3,6 +3,5 @@ export {default as ContactForm} from './ContactForm'
 export {default as Footer} from './Footer'
 export {default as TopHeader} from './TopHeader'
 export {default as lome} from '../components/svg/lome.svg'
-export {default as lome2} from '../components/assets/logo.png'
 export {default as whatsapp} from '../components/svg/whatsapp.svg'
 
