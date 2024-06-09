217d4b20e1db0c70f2a718a9cc0435e94418da07
shar27 <shariq.ahmed731@gmail.com>
2024-06-09 14:43:32 +0200

    top header format

--- components/TopHeader.js
+++ components/TopHeader.js
@@ -6,11 +6,10 @@ import { whatsapp } from './Imports'
 function TopHeader() {
   return (
     <div className="w-full bg-black text-white font-bold flex flex-col lg:flex-row justify-center 
-    lg:justify-between p-2 text-xl">
+    lg:justify-between p-2 text-lg">
        
       <Link href="mailto:hello@webdesignhighwycombe.co.uk">
- 
-📧 hello@webdesignhighwycombe.co.uk
+        📧hello@webdesignhighwycombe.co.uk
       </Link>
    
 <Link href="tel:0333 360 6113">
