e3e899166cc277b4e269315e7a878eb7634bb402
shar27 <shariq.ahmed731@gmail.com>
2024-06-11 10:50:54 +0200

    sitemap edit

--- pages/Sitemap.js
+++ pages/Sitemap.js
@@ -1,4 +1,6 @@
-export default function sitemap() {
+// pages/sitemap.js
+
+export function Sitemap() {
   return [
     {
       url: 'https://www.webdesignhighwycombe.co.uk/',
@@ -15,38 +17,42 @@ export default function sitemap() {
     {
       url: 'https://www.webdesignhighwycombe.co.uk/Services',
       lastModified: new Date('2024-06-08T21:34:21+00:00'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
     {
       url: 'https://www.webdesignhighwycombe.co.uk/posts/house-clearance-wales',
       lastModified: new Date('2024-06-08T21:34:21+00:00'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
     {
       url: 'https://www.webdesignhighwycombe.co.uk/posts/house%20of%20computers',
       lastModified: new Date('2024-06-08T21:34:21+00:00'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
     {
       url: 'https://www.webdesignhighwycombe.co.uk/posts/london-cleaning-services',
       lastModified: new Date('2024-06-08T21:34:21+00:00'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
     {
       url: 'https://www.webdesignhighwycombe.co.uk/posts/the%20hive',
       lastModified: new Date('2024-06-08T21:34:21+00:00'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      changeFrequency: 'weekly',
+      priority: 0.5,
     },
     {
       url: 'https://www.webdesignhighwycombe.co.uk/Terms',
       lastModified: new Date('2024-06-08T21:34:21+00:00'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    }
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
   ];
+}
+
+export default function Sitemap() {
+  return null;
 }
