2fce5b605895d70d3dabebe16ccfae57c5e440f2
shar27 <shariq.ahmed731@gmail.com>
2024-06-13 15:44:30 +0200

    reCaptcha

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,24 +1,42 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 
 export default function ContactUs () {
   const form = useRef();
   const [text, setText] = useState('')
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    if (!recaptchaValue) {
+      setText('Please complete the reCAPTCHA');
+      return;
+    }
+
+    setIsSubmitting(true);
 
     // process.env.YOUR_TEMPLATE_ID
     emailjs.sendForm('service_39bg1xq', 'template_pju0m5e', form.current, '_zvfCNAUf1DOGw6zi')
       .then((result) => {
           console.log(result.text);
           setText('Thank you, your email has been received!')
+          setIsSubmitting(false);
+          setRecaptchaValue(null);
       }, (error) => {
+
           console.log(error.text);
+          setIsSubmitting(false);
       });
   };  
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+  };
+
 
   return (
    
@@ -40,6 +58,12 @@ export default function ContactUs () {
       <textarea required className='w-full h-52  border-b-2 mb-5' 
       name="message" />
       <br/>
+      <div className='mt-5'>
+          <ReCAPTCHA
+            sitekey="6Lei__cpAAAAAMULu7A30dptnQTV7pJEHhvrpg3t" // Replace with your reCAPTCHA site key
+            onChange={handleRecaptchaChange}
+          />
+        </div>
       <div className='mt-auto'>
       <input className='w-full p-4 bg-customBlue text-white font-bold  cursor-pointer' 
       type="submit" value="Send" />
